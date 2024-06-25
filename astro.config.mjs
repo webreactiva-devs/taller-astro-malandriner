c9f38271e73f5e485fda2243d6a2cdfb3ac70f86
Daniel Primo <delineas@gmail.com>
2024-06-25 22:30:15 +0200

    After npx astro add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), db()]
+  integrations: [mdx(), sitemap(), tailwind(), db()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
