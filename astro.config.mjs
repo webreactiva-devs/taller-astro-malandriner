9b73665f0aa22e7c6aec5cfed58d17999007e4eb
Daniel Primo <delineas@gmail.com>
2024-06-25 22:25:47 +0200

    Automatic changes after npx add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), db()]
 });
